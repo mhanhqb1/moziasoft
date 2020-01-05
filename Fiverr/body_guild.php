47101d75f97fe460e3926eeeac83f3ea068ff1dc
AnhMH <anh.mh@moziasoft.com>
2020-01-05 11:36:23 +0700

    Modify guides

---
+++ Fiverr/body_guild.php
@@ -0,0 +1,26 @@
+<div class="fiverr-guides" style="">
+    <h2>Guides</h2>
+    <div class="wrapper">
+        <a href="#" class="guide">
+            <img alt="Create a Website" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_450,dpr_1.0/general_assets/logged_out_homepage/assets/fiverr_guides/1440-create-website-2x.jpg" loading="lazy">
+            <div class="content">
+                <h6>Create a Website</h6>
+                <p>Building a stunning website from A to Z Building a stunning website from A to Z</p>
+            </div>
+        </a>
+        <a href="#" class="guide">
+            <img alt="Create a Website" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_450,dpr_1.0/general_assets/logged_out_homepage/assets/fiverr_guides/1440-create-website-2x.jpg" loading="lazy">
+            <div class="content">
+                <h6>Grow With Digital Marketing</h6>
+                <p>Promoting your business online Promoting your business online</p>
+            </div>
+        </a>
+        <a href="#" class="guide">
+            <img alt="Create a Website" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_450,dpr_1.0/general_assets/logged_out_homepage/assets/fiverr_guides/1440-create-website-2x.jpg" loading="lazy">
+            <div class="content">
+                <h6>Build a Strong Brand</h6>
+                <p>Differentiating yourself from the competition Differentiating yourself from the competition</p>
+            </div>
+        </a>
+    </div>
+</div>
