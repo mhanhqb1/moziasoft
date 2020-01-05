814c4271623eeafc5f20190c2c026816935bd351
AnhMH <anh.mh@moziasoft.com>
2020-01-05 11:27:11 +0700

    Modify body categories

--- Fiverr/index.php
+++ Fiverr/index.php
@@ -48,35 +48,7 @@ $version = time();
                             <?php require_once 'body_slider1.php'; ?>
 
                             <!--Categories grids-->
-                            <div class="main-categories">
-                                <h2>Explore The Marketplace</h2>
-                                <ul class="categories-list">
-                                    <li>
-                                        <a href="#"><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/graphics-design.d32a2f8.svg" alt="Graphics &amp; Design" loading="lazy">Graphics &amp; Design</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/online-marketing.74e221b.svg" alt="Digital Marketing" loading="lazy">Digital Marketing</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/writing-translation.32ebe2e.svg" alt="Writing &amp; Translation" loading="lazy">Writing &amp; Translation</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/video-animation.f0d9d71.svg" alt="Video &amp; Animation" loading="lazy">Video &amp; Animation</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/music-audio.320af20.svg" alt="Music &amp; Audio" loading="lazy">Music &amp; Audio</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/programming.9362366.svg" alt="Programming &amp; Tech" loading="lazy">Programming &amp; Tech</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/business.bbdf319.svg" alt="Business" loading="lazy">Business</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/lifestyle.745b575.svg" alt="Lifestyle" loading="lazy">Lifestyle</a>
-                                    </li>
-                                </ul>
-                            </div>
+                            <?php require_once 'body_categories.php'; ?>
 
                             <!--Guilds-->
                             <div class="fiverr-guides" style="">
