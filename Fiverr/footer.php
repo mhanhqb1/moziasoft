c081d034cedf6f22f7901446ae8daacb1fd3e9c5
AnhMH <anh.mh@moziasoft.com>
2020-01-05 12:00:21 +0700

    Modify footer

---
+++ Fiverr/footer.php
@@ -0,0 +1,271 @@
+<div id="Footer-component" class="">
+    <footer data-reactroot="" class="footer-package site-footer">
+        <div class="footer-wrapper max-width-container">
+            <div class="footer-collapsibles grid-15">
+                <div class="col-15-xs col-5-sm col-3-md">
+                    <article class="fit-collapsible footer-collapsible categories">
+                        <div class="fit-collapsible-title-wrapper">
+                            <div class="fit-collapsible-title">
+                                <h6 class="text-display-7">Categories</h6></div>
+                        </div>
+                        <div class="fit-collapsible-content">
+                            <ul>
+                                <li><a href="#">Graphics &amp; Design</a></li>
+                                <li><a href="#">Digital Marketing</a></li>
+                                <li><a href="#">Writing &amp; Translation</a></li>
+                                <li><a href="#">Video &amp; Animation</a></li>
+                                <li><a href="#">Music &amp; Audio</a></li>
+                                <li><a href="#">Programming &amp; Tech</a></li>
+                                <li><a href="#">Business</a></li>
+                                <li><a href="#">Lifestyle</a></li>
+                                <li><a href="#">Sitemap</a></li>
+                            </ul>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-15-xs col-5-sm col-3-md">
+                    <article class="fit-collapsible footer-collapsible">
+                        <div class="fit-collapsible-title-wrapper">
+                            <div class="fit-collapsible-title">
+                                <h6 class="text-display-7">About</h6></div>
+                        </div>
+                        <div class="fit-collapsible-content">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 46 -->Careers
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 49 -->Press &amp; News
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 52 -->Partnerships
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 55 -->Privacy Policy
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 58 -->Terms of Service
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 61 -->Intellectual Property Claims
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 64 -->GIG8.de
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank">
+                                        <!-- react-text: 67 -->Investor Relations
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-15-xs col-5-sm col-3-md">
+                    <article class="fit-collapsible footer-collapsible">
+                        <div class="fit-collapsible-title-wrapper">
+                            <div class="fit-collapsible-title">
+                                <h6 class="text-display-7">Support</h6></div>
+                        </div>
+                        <div class="fit-collapsible-content">
+                            <ul>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 81 -->Help &amp; Support
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 84 -->Trust &amp; Safety
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 87 -->Selling on GIG8
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 90 -->Buying on GIG8
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-15-xs col-5-sm col-3-md">
+                    <article class="fit-collapsible footer-collapsible">
+                        <div class="fit-collapsible-title-wrapper">
+                            <div class="fit-collapsible-title">
+                                <h6 class="text-display-7">Community</h6></div>
+                        </div>
+                        <div class="fit-collapsible-content">
+                            <ul>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 104 -->Events
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 107 -->Blog
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 110 -->Forum
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 113 -->Podcast
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 116 -->Affiliates
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 119 -->Invite a Friend
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 122 -->Become a Seller
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-15-xs col-5-sm col-3-md">
+                    <article class="fit-collapsible footer-collapsible">
+                        <div class="fit-collapsible-title-wrapper">
+                            <div class="fit-collapsible-title">
+                                <h6 class="text-display-7">More From GIG8</h6></div>
+                        </div>
+                        <div class="fit-collapsible-content">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 136 -->GIG8 Pro
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <!-- react-text: 139 -->GIG8 Studios
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 142 -->Get Inspired
+                                        <!-- /react-text -->
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 145 -->ClearVoice
+                                        <!-- /react-text --><small>Content Marketing</small></a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 149 -->AND CO
+                                        <!-- /react-text --><small>Invoice Software</small></a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 153 -->Learn
+                                        <!-- /react-text --><small>Online Courses</small></a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener">
+                                        <!-- react-text: 157 -->GIG8 Elevate
+                                        <!-- /react-text --><small>Exclusive Benefits</small></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="bottom">
+                <div class="legal"><span class="logo">GIG8</span>
+                    <p class="text-body-2"><span class="copyright"><!-- react-text: 164 -->© <!-- /react-text --><!-- react-text: 165 -->GIG8 International Ltd. 2020<!-- /react-text --></span></p>
+                </div>
+                <div class="currency-social">
+                    <ul class="social">
+                        <li class="twitter"><a href="#" target="_blank" rel="noopener">GIG8 on Twitter</a></li>
+                        <li class="facebook"><a href="#" target="_blank" rel="noopener">GIG8 on Facebook</a></li>
+                        <li class="linkedin"><a href="#" target="_blank" rel="noopener">GIG8 on LinkedIn</a></li>
+                        <li class="pinterest"><a href="#" target="_blank" rel="noopener">GIG8 on Pinterest</a></li>
+                        <li class="instagram"><a href="#" target="_blank" rel="noopener">GIG8 on Instagram</a></li>
+                    </ul>
+                    <div class="currency-selector">
+                        <select>
+                            <option value="USD">United States Dollar - $</option>
+                            <option value="EUR">Euro - €</option>
+                            <option value="GBP">British Pound - £</option>
+                            <option value="AUD">Australian Dollar - A$</option>
+                            <option value="CAD">Canadian Dollar - C$</option>
+                            <option value="ILS">Israeli Shekel - ₪</option>
+                            <option value="BRL">Brazilian Real - R$</option>
+                            <option value="HKD">Hong Kong Dollar - HK$</option>
+                            <option value="SEK">Swedish Krona - kr</option>
+                            <option value="NZD">New Zealand Dollar - NZ$</option>
+                            <option value="SGD">Singapore Dollar - S$</option>
+                            <option value="CHF">Swiss Franc - CHF</option>
+                            <option value="ZAR">South African Rand - R</option>
+                            <option value="CNY">Chinese Renminbi Yuan - ¥</option>
+                            <option value="INR">Indian Rupee - ₹</option>
+                            <option value="MYR">Malaysian Ringgit - RM</option>
+                            <option value="MXN">Mexican Peso - $</option>
+                            <option value="PKR">Pakistani Rupee - ₨</option>
+                            <option value="PHP">Philippine Peso - ₱</option>
+                            <option value="TWD">New Taiwan Dollar - $</option>
+                            <option value="THB">Thai Baht - ฿</option>
+                            <option value="TRY">Turkish New Lira - ₺</option>
+                            <option value="AED">United Arab Emirates Dirham - د.إ</option>
+                        </select>
+                        <label>USD - $</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
